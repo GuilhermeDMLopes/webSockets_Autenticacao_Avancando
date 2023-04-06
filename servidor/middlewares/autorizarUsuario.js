b67ca8373227dd778e279b8a42435cd74d3136e1
guilherme <guilhermedmlopes@hotmail.com>
2023-04-06 20:10:57 -0300

    Removing comments

--- servidor/middlewares/autorizarUsuario.js
+++ servidor/middlewares/autorizarUsuario.js
@@ -4,12 +4,8 @@ function autorizarUsuario(socket, next) {
     const tokenJwt = socket.handshake.auth.token;
 
     try {
-        //Vamos pegar o payload de retorno da função verify  e mandar para o frontend
         const payloadToken = jwt.verify(tokenJwt, process.env.SEGREDO_JWT)
 
-        //emitindo payload para front. Lembrando que o payload é um objeto com nome do usuario
-        //Essa função é executada para toda pagina restrita, ela sera executada para quem estiver entrando
-        //em uma pagina de documento específico
         socket.emit("autorizacao_sucesso", payloadToken)
         next()
     } catch (erro) {
--- servidor/servidor.js
+++ servidor/servidor.js
@@ -22,21 +22,5 @@ const io = new Server(servidorHttp);
 export default io;
 
 /*
-Iremos agora mostrar qual usuário está conectado na página de cada documento
 
-Para fazer isso, precisaremos fazer algumas alterações no middleware de autorizarUsuario.js.
-
-Feita a alteração, iremos receber o payload em socket-front-documento.
-
-Agora ja estamos recebendo o nome do usuario no servidor que está conectado na pagina de documento.
-Vamos manter um registro de qual usuario acessou tal documento. Criaremos uma lista local do node em documento.js
-para o frontend consumir e apresentar na tela. Iremos criar dentro do diretorio utils no servidor o arquivo
-conexoesDocumentos.js.
-
-Criamos uma lista para receber as conexoes de cada documento com o nome de cada usuario que está nele.
-Agora faremos a atualização dos usuarios conectados no frontend em socket-front-documento.js.
-
-Depois de implementado a lista de usuarios conectados na mesma conexão, precisaremos corrigir um problema.
-Por enquanto estamos controloando apenas a entrada de usuarios. Porém, se eu sair e voltar para o documento,
-meu usuario aparecerá 2x. Sera feita essa implementação na branch seguinte.
 */
